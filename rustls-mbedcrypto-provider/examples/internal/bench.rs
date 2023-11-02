bfbe0fdeb6407bc07a5544b4904563e52cf16aa4
Yuxiang Cao <yuxiang.cao@fortanix.com>
2023-11-01 17:18:45 -0700

    chore: add copyright header

--- rustls-mbedcrypto-provider/examples/internal/bench.rs
+++ rustls-mbedcrypto-provider/examples/internal/bench.rs
@@ -1,3 +1,10 @@
+/* Copyright (c) Fortanix, Inc.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 // This program does assorted benchmarking of rustls.
 //
 // Note: we don't use any of the standard 'cargo bench', 'test::Bencher',
--- rustls-mbedcrypto-provider/src/lib.rs
+++ rustls-mbedcrypto-provider/src/lib.rs
@@ -1,3 +1,10 @@
+/* Copyright (c) Fortanix, Inc.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 //! rustls-mbedcrypto-provider
 //!
 //! rustls-mbedcrypto-provider is a crypto provider for rustls based on [mbedtls].
