91ee6ecb66e6279b63e719ae7a1825a1a88231ea
Arash Sahebolamri <arash.sahebolamri@gmail.com>
2023-12-19 15:01:21 -0800

    Implement FFDHE support in mbedcrypto-provider (#23)
    * Implement FFDHE support in mbedcrypto-provider
    
    * Add test for bad FFDHE public keys
    
    * Address review comments
    
    * Update rustls dependency to one based on 0.22.1 in Cargo.lock
    
    ---------
    
    Co-authored-by: Arash Sahebolamri <arash.sahebolamri@fortanix.com>

--- rustls-mbedcrypto-provider/src/kx.rs
+++ rustls-mbedcrypto-provider/src/kx.rs
@@ -5,6 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+use std::sync::Mutex;
 use std::sync::OnceLock;
 
 use super::agreement;
@@ -15,11 +16,15 @@ use alloc::fmt;
 use alloc::format;
 use alloc::vec::Vec;
 use crypto::SupportedKxGroup;
+use mbedtls::bignum::Mpi;
+use mbedtls::rng::Random;
 use mbedtls::{
     ecp::EcPoint,
     pk::{EcGroup, Pk as PkMbed},
 };
 use rustls::crypto;
+use rustls::crypto::ActiveKeyExchange;
+use rustls::ffdhe_groups;
 use rustls::Error;
 use rustls::NamedGroup;
 /// A key-exchange group supported by *mbedtls*.
@@ -41,7 +46,7 @@ impl fmt::Debug for KxGroup {
 }
 
 impl SupportedKxGroup for KxGroup {
-    fn start(&self) -> Result<Box<dyn crypto::ActiveKeyExchange>, Error> {
+    fn start(&self) -> Result<Box<dyn ActiveKeyExchange>, Error> {
         let priv_key = generate_ec_key(self.agreement_algorithm.group_id)?;
 
         Ok(Box::new(KeyExchange {
@@ -78,8 +83,33 @@ pub static SECP384R1: &dyn SupportedKxGroup =
 pub static SECP521R1: &dyn SupportedKxGroup =
     &KxGroup { name: NamedGroup::secp521r1, agreement_algorithm: &agreement::ECDH_P521 };
 
+/// DHE group [FFDHE2048](https://www.rfc-editor.org/rfc/rfc7919.html#appendix-A.1)
+pub static FFDHE2048: &dyn SupportedKxGroup =
+    &DheKxGroup { name: NamedGroup::FFDHE2048, group: ffdhe_groups::FFDHE2048, priv_key_len: 36 };
+
+/// DHE group [FFDHE3072](https://www.rfc-editor.org/rfc/rfc7919.html#appendix-A.2)
+pub static FFDHE3072: &dyn SupportedKxGroup =
+    &DheKxGroup { name: NamedGroup::FFDHE3072, group: ffdhe_groups::FFDHE3072, priv_key_len: 40 };
+
+/// DHE group [FFDHE4096](https://www.rfc-editor.org/rfc/rfc7919.html#appendix-A.3)
+pub static FFDHE4096: &dyn SupportedKxGroup =
+    &DheKxGroup { name: NamedGroup::FFDHE4096, group: ffdhe_groups::FFDHE4096, priv_key_len: 48 };
+
+/// DHE group [FFDHE6144](https://www.rfc-editor.org/rfc/rfc7919.html#appendix-A.4)
+pub static FFDHE6144: &dyn SupportedKxGroup =
+    &DheKxGroup { name: NamedGroup::FFDHE6144, group: ffdhe_groups::FFDHE6144, priv_key_len: 56 };
+
+/// DHE group [FFDHE8192](https://www.rfc-editor.org/rfc/rfc7919.html#appendix-A.5)
+pub static FFDHE8192: &dyn SupportedKxGroup =
+    &DheKxGroup { name: NamedGroup::FFDHE8192, group: ffdhe_groups::FFDHE8192, priv_key_len: 64 };
+
 /// A list of all the key exchange groups supported by mbedtls.
-pub static ALL_KX_GROUPS: &[&dyn SupportedKxGroup] = &[X25519, SECP256R1, SECP384R1, SECP521R1];
+pub static ALL_KX_GROUPS: &[&dyn SupportedKxGroup] = &[
+    // ECDHE groups:
+    X25519, SECP256R1, SECP384R1, SECP521R1, // fmt
+    // (FF)DHE groups:
+    FFDHE2048, FFDHE3072, FFDHE4096, FFDHE6144, FFDHE8192,
+];
 
 /// An in-progress key exchange.  This has the algorithm,
 /// our private key, and our public key.
@@ -102,7 +132,7 @@ impl KeyExchange {
     }
 }
 
-impl crypto::ActiveKeyExchange for KeyExchange {
+impl ActiveKeyExchange for KeyExchange {
     /// Completes the key exchange, given the peer's public key.
     fn complete(mut self: Box<Self>, peer_public_key: &[u8]) -> Result<crypto::SharedSecret, Error> {
         let group_id = self.agreement_algorithm.group_id;
@@ -135,6 +165,103 @@ impl crypto::ActiveKeyExchange for KeyExchange {
     }
 
     /// Return the group being used.
+    fn group(&self) -> NamedGroup {
+        self.name
+    }
+}
+
+#[derive(Debug)]
+struct DheKxGroup {
+    name: NamedGroup,
+    group: ffdhe_groups::FfdheGroup<'static>,
+    priv_key_len: usize,
+}
+
+impl SupportedKxGroup for DheKxGroup {
+    fn start(&self) -> Result<Box<dyn ActiveKeyExchange>, Error> {
+        let g = mbedtls::bignum::Mpi::from_binary(self.group.g).map_err(mbedtls_err_to_rustls_error)?;
+        let p = mbedtls::bignum::Mpi::from_binary(self.group.p).map_err(mbedtls_err_to_rustls_error)?;
+
+        let mut rng = super::rng::rng_new().ok_or(rustls::crypto::GetRandomFailed)?;
+        let mut x = vec![0; self.priv_key_len];
+        rng.random(&mut x)
+            .map_err(|_| rustls::crypto::GetRandomFailed)?;
+        let x = mbedtls::bignum::Mpi::from_binary(&x)
+            .map_err(|e| Error::General(format!("failed to make Bignum from random bytes: {}", e)))?;
+
+        let x_pub = g
+            .mod_exp(&x, &p)
+            .map_err(mbedtls_err_to_rustls_error)?;
+
+        Ok(Box::new(DheActiveKeyExchange {
+            name: self.name,
+            p: Mutex::new(p),
+            x: Mutex::new(x),
+            x_pub: x_pub
+                .to_binary()
+                .map_err(mbedtls_err_to_rustls_error)?,
+        }))
+    }
+
+    fn name(&self) -> NamedGroup {
+        self.name
+    }
+}
+
+struct DheActiveKeyExchange {
+    name: NamedGroup,
+    // Using Mutex just because `Mpi` is not currently `Sync`
+    // TODO remove the Mutex once we switch to a version of mbedtls where `Mpi` is `Sync`
+    p: Mutex<Mpi>,
+    x: Mutex<Mpi>,
+    x_pub: Vec<u8>,
+}
+
+impl ActiveKeyExchange for DheActiveKeyExchange {
+    fn complete(self: Box<Self>, peer_pub_key: &[u8]) -> Result<crypto::SharedSecret, Error> {
+        let y_pub = Mpi::from_binary(peer_pub_key).map_err(mbedtls_err_to_rustls_error)?;
+
+        let x = self
+            .x
+            .into_inner()
+            .expect("Mpi Mutex poisoned");
+        let p = self
+            .p
+            .into_inner()
+            .expect("Mpi Mutex poisoned");
+
+        let one = Mpi::new(1).map_err(mbedtls_err_to_rustls_error)?;
+
+        let mut p_minus_one = p;
+        p_minus_one -= &one;
+
+        // https://www.rfc-editor.org/rfc/rfc7919.html#section-5.1:
+        // Peers MUST validate each other's public key Y [...] by ensuring that 1 < Y < p-1.
+        if !(one < y_pub && y_pub < p_minus_one) {
+            return Err(Error::General(
+                "Invalid DHE key exchange public key received; pub key must be in range (1, p-1)".into(),
+            ));
+        }
+
+        p_minus_one += &one;
+        let p = p_minus_one;
+
+        let secret = y_pub
+            .mod_exp(&x, &p)
+            .map_err(mbedtls_err_to_rustls_error)?;
+
+        Ok(crypto::SharedSecret::from(
+            secret
+                .to_binary()
+                .map_err(mbedtls_err_to_rustls_error)?
+                .as_ref(),
+        ))
+    }
+
+    fn pub_key(&self) -> &[u8] {
+        &self.x_pub
+    }
+
     fn group(&self) -> NamedGroup {
         self.name
     }
--- rustls-mbedcrypto-provider/src/lib.rs
+++ rustls-mbedcrypto-provider/src/lib.rs
@@ -193,12 +193,19 @@ pub static ALL_CIPHER_SUITES: &[SupportedCipherSuite] = &[
     tls12::TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
     #[cfg(feature = "tls12")]
     tls12::TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+    #[cfg(feature = "tls12")]
+    tls12::TLS_DHE_RSA_WITH_AES_128_GCM_SHA256,
+    #[cfg(feature = "tls12")]
+    tls12::TLS_DHE_RSA_WITH_AES_256_GCM_SHA384,
+    #[cfg(feature = "tls12")]
+    tls12::TLS_DHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
 ];
 
 /// All defined cipher suites supported by *mbedtls* appear in this module.
 pub mod cipher_suite {
     #[cfg(feature = "tls12")]
     pub use super::tls12::{
+        TLS_DHE_RSA_WITH_AES_128_GCM_SHA256, TLS_DHE_RSA_WITH_AES_256_GCM_SHA384, TLS_DHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
         TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256, TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
         TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256, TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
         TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384, TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
@@ -249,6 +256,12 @@ pub static SUPPORTED_SIG_ALGS: WebPkiSupportedAlgorithms = WebPkiSupportedAlgori
 ///
 /// [`ALL_KX_GROUPS`] is provided as an array of all of these values.
 pub mod kx_group {
+    pub use super::kx::FFDHE2048;
+    pub use super::kx::FFDHE3072;
+    pub use super::kx::FFDHE4096;
+    pub use super::kx::FFDHE6144;
+    pub use super::kx::FFDHE8192;
+
     pub use super::kx::SECP256R1;
     pub use super::kx::SECP384R1;
     pub use super::kx::SECP521R1;
